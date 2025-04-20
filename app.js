61537234b92ba02902b95daf53f7b80cde49c560
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-04-20 16:03:02 +0200

    ejercicios 1-2-3

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// Cargar los libros desde el archivo JSON
+const booksPath = path.join(__dirname, "data", "books.json");
+let books = [];
+
+try {
+  const booksData = fs.readFileSync(booksPath, "utf8");
+  books = JSON.parse(booksData);
+} catch (err) {
+  console.error("Error al leer el archivo books.json:", err);
+}
+
+// Crea una ruta /all para obtener todos los libros
+app.get("/books/all", (req, res) => {
+  res.json(books);
+});
+
+//Crea una ruta /first para obtener el primer libro
+app.get("/books/first", (req, res) => {
+  res.json(books[0]);
+});
+
+//Crea una ruta /last para obtener el último libro
+app.get("/books/last", (req, res) => {
+  res.json(books[books.length - 1]);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});

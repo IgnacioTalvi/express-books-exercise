f754ca260675e335485e5d9f3b0ebf11e63d3043
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-04-27 17:08:44 +0200

    add/excercise4-5

--- repaso.js
+++ repaso.js
@@ -31,6 +31,20 @@ app.get("books/last", (req, res) => {
 });
 
 // Ejercicio 4 - Crea una ruta /middle para obtener el libro en la mitad (número 50 en el array)
+app.get("books/middle", (req, res) => {
+  res.json(books);
+});
+
+// Ejercicio 5 - Crea una ruta /author/dante-alighieri para obtener SÓLO EL TÍTULO del libro de Dante Alighieri
+app.get("author/dante-alighieri", (req, res) => {
+const danteBook = books.find (book) = book.title === "Dante Alighieri";
+    if (danteBook) {
+        res.status(200).json(danteBook); // devuelve el libro con el título Harry Potter
+    } else {
+      // si el libro no existe
+      res.status(404).json({ msj: "Libro no encontrado" }); // devuelve un mensaje de error
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
